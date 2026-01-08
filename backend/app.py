09a167d227737336c95867401ac92416e2722774
emmanuel10101 <100emmanuel001@gmail.com>
2026-01-08 11:11:10 -0500

    Finished delete user API and added some rate limiting, idk if I'm going to add it to every API,  also going to start working on the frontend so I installed some chakraUI stuff

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,19 @@ from flask import Flask, jsonify, make_response, request
 import sqlite3
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+CORS(app)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"] # Default limits for all routes
+)
 
-# This function returns a connection to the database which can be used to send SQL commands to the database
+# Returns a connection to the database which can be used to send SQL commands to the database
 def get_db_connection():
   conn = sqlite3.connect('../database/tessera.db')
   conn.row_factory = sqlite3.Row
@@ -35,12 +44,13 @@ def get_events():
     # Convert the rows to dictionaries to make them serializable
     events_list = [dict(event) for event in events]
     
-    conn.close()
+    conn.close() # Close the database connection
     
     return jsonify(events_list)
 
 # Endpoint for creating a new user
-@app.route('/user', methods=['POST'])
+@app.route('/users', methods=['POST'])
+@limiter.limit("5 per minute")
 def create_user():
     # Extract email, username, and password from the JSON payload
     email = request.json.get('email')
@@ -52,7 +62,7 @@ def create_user():
         return jsonify({'error': 'All fields (email, username, and password) are required.'}), 400
 
     # Hash the password
-    hashed_password = hashlib.sha256(password.encode()).hexdigest() # not using generate_password_hash here to test why its not matching
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
     try:
         conn = get_db_connection()
@@ -66,7 +76,6 @@ def create_user():
         # Retrieve the user_id of the newly created user to confirm creation
         cursor.execute('SELECT user_id FROM Users WHERE username = ?', (username,))
         new_user_id = cursor.fetchone()
-
         conn.close()
 
         return jsonify({'message': 'User created successfully', 'user_id': new_user_id['user_id']}), 201
@@ -77,17 +86,16 @@ def create_user():
         return jsonify({'error': str(e)}), 500
 
 # Endpoint for user login
-@app.route('/user', methods=['GET'])
+@app.route('/users/login', methods=['GET'])
 def user_login():
     username = request.json.get('username')
     password = request.json.get('password')
 
-    # Basic validation to ensure all fields are provided
     if not username or not password:
         return jsonify({'error': 'All fields (username and password) are required.'}), 400
 
     # Hash the password
-    hashed_password_input = hashlib.sha256(password.encode()).hexdigest() # not using generate_password_hash here to test why its not matching
+    hashed_password_input = hashlib.sha256(password.encode()).hexdigest()
 
     try:
         conn = get_db_connection()
@@ -99,9 +107,10 @@ def user_login():
         # Retrieve hashed password from the database
         cursor.execute('SELECT password_hash from Users WHERE username = ?',(username,))
         hash_from_db = cursor.fetchone()
-
         conn.close()
-        if hashed_password_input == hash_from_db['password_hash']: # not using check_password_hash here to test why its not matching
+
+        # Not using check_password_hash here to test why its not matching
+        if hashed_password_input == hash_from_db['password_hash']:
             return jsonify({'message': 'Login successful'}), 200
         else:
             return jsonify({'error': 'Invalid password.'}), 401
@@ -110,18 +119,71 @@ def user_login():
         print(e)
         return jsonify({'error': "Username not found"}), 500
 
-@app.route('/user', methods=['PUT'])
-def change_username_or_password():
-    return jsonify({'message': 'Alter user endpoint - to be implemented'}), 200
+# Endpoint for changing user password
+@app.route('/users', methods=['PUT'])
+def change_password():
+    username = request.json.get('username')
+    new_password = request.json.get('new_password')
 
-@app.route('/user', methods=['DELETE'])
+    # Hash the new password
+    hashed_password_input = hashlib.sha256(new_password.encode()).hexdigest()
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Retrieve current password from the database
+        cursor.execute('SELECT password_hash from Users WHERE username = ?',(username,))
+        hash_from_db = cursor.fetchone()
+
+        if hashed_password_input == hash_from_db['password_hash']:
+            return jsonify({'error': 'New password matches current password'}), 400
+        else:
+            cursor.execute('UPDATE Users SET password_hash = ? WHERE username = ?',
+                           (hashed_password_input, username))
+            conn.commit()  # Commit the changes to the database
+            conn.close()
+            return jsonify({'message': 'Password changed successfully'}), 201
+
+    except Exception as e:
+        print(e)
+        return jsonify({'error': "you dont exist or sumn"}), 500
+
+# Endpoint for deleting a user
+@app.route('/users', methods=['DELETE'])
 def delete_user():
-    return jsonify({'message': 'Delete user endpoint - to be implemented'}), 200
 
-@app.route('/event', methods=['POST'])
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    # Hash the new password
+    hashed_password_input = hashlib.sha256(password.encode()).hexdigest()
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Retrieve current password from the database
+        cursor.execute('SELECT password_hash from Users WHERE username = ?',(username,))
+        hash_from_db = cursor.fetchone()
+
+        if hashed_password_input != hash_from_db['password_hash']:
+            return jsonify({'error': 'Password does not match'}), 400
+        else:
+            cursor.execute('DELETE FROM Users WHERE username = ?', (username,))
+            conn.commit()
+            conn.close()
+            return jsonify({'message': 'User deleted successfully'}), 201
+    
+    except Exception as e:
+        print(e)
+        return jsonify({'error': "you dont exist or sumn"}), 500
+
+@app.route('/events', methods=['POST'])
 def create_event():
     return jsonify({'message': 'Create event endpoint - to be implemented'}), 200
 
+# Endpoint for getting all user emails
 @app.route('/emails', methods=['GET'])
 def get_emails():
     conn = get_db_connection()
@@ -129,8 +191,7 @@ def get_emails():
     
     # Start with the base SQL query and fill emails list
     query = 'SELECT email FROM Users'
-    params = []
-    cursor.execute(query, params)
+    cursor.execute(query)
     emails = cursor.fetchall()
     
     # Convert the rows to dictionaries to make them serializable
@@ -138,6 +199,10 @@ def get_emails():
     conn.close()
     
     return jsonify(emails_list), 200
+
+@app.route('/events', methods=['POST'])
+def create_event():
+    return jsonify({'message': 'Create event endpoint - to be implemented'}), 200
 
 @app.route('/award_ticket', methods=['POST'])
 def award_ticket():
